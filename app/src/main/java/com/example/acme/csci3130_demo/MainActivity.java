8c98067a1f1539c73aba91dc3043285d6f8e737d
Sai Kishore Eppalagudem <saikishore.eppalagudem@gmail.com>
2018-01-21 19:26:23 -0400

    Added UI elements and button click functionality

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.acme.csci3130_demo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    TextView tv;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.editText);
+        tv = findViewById(R.id.textView);
+        btn =  findViewById(R.id.button);
+    }
+
+    public void btnClick(View view) {
+        tv.setText(et.getText());
+
     }
 }
